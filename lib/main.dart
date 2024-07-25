626f5464c7544d1c9dcc15b133cbe1554d109d5f
darkarnold <arnoldndiwalana@gmail.com>
2024-07-25 16:49:13 +0300

    setup hello world flutter app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text(
+        'Hello World!',
+        style: TextStyle(fontSize: 24, color: Colors.green),
+      ),
+    );
+  }
+}
